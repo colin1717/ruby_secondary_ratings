83757b570ec560e7f84c3ab8c630094ad995f2d7
colin1717 <colin1717@mac.com>
2016-12-14 17:37:07 -0600

    if statement sorts reviewIds correctly

--- first.rb
+++ first.rb
@@ -12,21 +12,36 @@ puts csv_array.inspect
 dimensionExternalId = {}
 
 csv_array.each do |row|
+  # puts "row[0]: #{row[0]}"
+  # puts "row[1]: #{row[1]}"
+  # puts "row[2]: #{row[2]}"
+  #
+  # puts "dimensionExternalId: #{dimensionExternalId.inspect}"
   if dimensionExternalId.key?(row[1])
-    dimensionExternalId[row[1]].push(row[0])
+    puts "dimensionExternalId[row[1]]: #{dimensionExternalId[row[1]]}"
+    if dimensionExternalId[row[1]].key?(row[2])
+      puts 'working up top'
+      dimensionExternalId[(row[1])][(row[2])].push(row[0])
+    else
+      puts "creating array for #{dimensionExternalId[row[1]][row[2]]}"
+      dimensionExternalId[row[1]][row[2]] = []
+      dimensionExternalId[row[1]][row[2]].push(row[0])
+    end
   else
-    dimensionExternalId[row[1]] = []
-    dimensionExternalId[row[1]].push(row[0])
+    puts "creating #{dimensionExternalId[row[1]]} hash, array, and pushing #{row[0]}"
+    dimensionExternalId[row[1]] = {}
+    dimensionExternalId[row[1]][row[2]] = []
+    dimensionExternalId[row[1]][row[2]].push(row[0])
   end
 end
 
 puts dimensionExternalId.inspect
 
-#loop through dimensionExternalId hash and create a file for each key.  Then write each reviewID from that dimensionExternalId in the file
-dimensionExternalId.each do |dimensionExternalIdKey, reviewArray|
-  outputFile = File.open("#{dimensionExternalIdKey}.txt", "w")
-  dimensionExternalId["#{dimensionExternalIdKey}"].each do |review|
-    outputFile.puts(review)
-  end
-  outputFile.close
-end
+# #loop through dimensionExternalId hash and create a file for each key.  Then write each reviewID from that dimensionExternalId in the file
+# dimensionExternalId.each do |dimensionExternalIdKey, reviewArray|
+#   outputFile = File.open("#{dimensionExternalIdKey}.txt", "w")
+#   dimensionExternalId["#{dimensionExternalIdKey}"].each do |review|
+#     outputFile.puts(review)
+#   end
+#   outputFile.close
+# end
